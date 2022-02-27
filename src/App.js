dbc17e7d70d9fef042c0d89f19b060817ea6c4d3
James Richards <jd-app@AppAcademyAir.home>
2022-02-27 14:12:56 -0500

    remove stuff

--- src/App.js
+++ src/App.js
@@ -3,19 +3,6 @@ function App() {
     <div>
       <h1>My App</h1>
       <p>Hello</p>
-      <p>Hello</p>
-      <p>Hello</p>
-      <p>Hello</p>
-      <p>Hello</p>
-      <p>Hello</p>
-      <p>Hello</p>
-      <span>Gooroo</span>
-      <span>Gooroo</span>
-      <span>Gooroo</span>
-      <span>Gooroo</span>
-      <span>Gooroo</span>
-      <span>Gooroo</span>
-      <span>Gooroo</span>
     </div>
   );
 }
